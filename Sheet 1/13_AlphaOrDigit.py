12b352a04439e40ddcb9496672f2cb459d3512c3
NKJ-718 <jhanitin16@gmail.com>
2025-11-29 22:54:30 +0530

    Day 4 progress

---
+++ Sheet 1/13_AlphaOrDigit.py
@@ -0,0 +1,7 @@
+a=input()
+if(ord(a)>=48 and ord(a)<=57):
+    print("IS DIGIT")
+elif(ord(a)>=65 and ord(a)<=90):
+    print("ALPHA\nIS CAPITAL")
+elif(ord(a)>=97 and ord(a)<=122):
+    print("ALPHA\nIS SMALL")
