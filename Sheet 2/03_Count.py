d15527adae269b3a9b61d3bd370a36e4a2d7d21f
NKJ-718 <jhanitin16@gmail.com>
2025-12-02 23:35:48 +0530

    Day 7 progress

---
+++ Sheet 2/03_Count.py
@@ -0,0 +1,19 @@
+n=int(input())
+lst=list(map(int,input().split()))
+odd=0
+even=0
+positive=0
+negative=0
+for i in lst:
+    if(i%2==0):
+        even+=1
+    else:
+        odd+=1
+    if(i>0):
+        positive+=1
+    elif(i<0):
+        negative+=1
+print("Even:",even)
+print("Odd:",odd)
+print("Positive:",positive)
+print("Negative:",negative)
